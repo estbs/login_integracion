a375e2174d66f57a6db020dda33aec9e1fed341b
alvaro-lopezg <67923538+alvaro-lopezg@users.noreply.github.com>
2023-09-05 19:53:45 -0500

    Add files via upload

---
+++ Integracion/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+
+users = {
+    'admin': 'password',
+    'alvaro': 'pass123',
+    'buitrete': 'buitre',
+    'alejandro': 'sapo',
+    'joha': 'clave123'
+}
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username in users and users[username] == password:
+            return redirect('/dashboard')
+        else:
+            message = 'Nombre de usuario o contraseña incorrectos'
+
+        return render_template('login.html', message=message)
+
+    return render_template('login.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    return '¡Bienvenido al panel de control!'
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
